72a56a6c5358213e126b7ee212ba17f0586b3ebb
Dila Rofika <dila.rofika45@gmail.com>
2022-05-27 13:29:41 +0700

    Product Pages

---
+++ frontend/src/pages/AdminPages/index.js
@@ -0,0 +1,14 @@
+import Dashboard from '../AdminPages/Dashboard.js'
+
+import AddProduct from '../AdminPages/Products/AddProduct.js'
+import EditProduct from '../AdminPages/Products/EditProduct.js'
+import ListProduct from '../AdminPages/Products/ListProduct.js'
+import DeleteProduct from '../AdminPages/Products/DeleteProduct.js'
+
+export {
+    Dashboard,
+    AddProduct,
+    EditProduct,
+    ListProduct,
+    DeleteProduct
+}
